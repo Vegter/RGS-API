7d3dbdfcd24f8cbf79f9fd3712955f9d22f5946c
vegter <amsjurien@gmail.com>
2021-04-12 15:42:06 +0200

    Add Swagger API documentation

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import { rootHandler } from './handlers'
 import { RGSHandler } from './rgs/handler'
 
+import { serve, setup } from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
+
 const app = express();
 const port = process.env.PORT || '8000';
 
+app.use('/api-docs', serve, setup(swaggerDocument));
 app.get('/', rootHandler);
 app.get('/rgs/:version?', RGSHandler)
 
